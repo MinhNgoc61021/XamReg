94fae67da1021aff3c96b94f95a026640a04c665
Communist217 <Needforspeed1900@gmail.com>
2019-11-11 18:26:36 +0700

    Update docker build

--- api/controller/__init__.py
+++ api/controller/__init__.py
@@ -1,12 +1,10 @@
 from flask import Flask
 from flask_cors import CORS
 from controller.authentication.auth import authentication
-from controller.test.test import tester
 from controller.authentication.auth import *
 
 def create_app():
     app = Flask(__name__)
     CORS(app)
     app.register_blueprint(authentication)
-    app.register_blueprint(tester)
     return app
