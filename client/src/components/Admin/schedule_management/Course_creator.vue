62bc1bd1033405808a4c87e8e1f7ad40479d3d63
Nam Pham <namphamc99@gmail.com>
2019-11-19 23:31:05 +0700

    Added Create course and shift components

---
+++ client/src/components/Admin/schedule_management/Course_creator.vue
@@ -0,0 +1,55 @@
+<template>
+    <section id="form-container">
+    <a-form :form="form" @submit="handleSubmit">
+    <a-form-item v-bind="formItemLayout" label="Mã kì thi: ">
+      <a-input style="width:200px"
+        v-decorator="[
+          'Mã kì thi: ',
+          {
+            rules: [
+              {
+                required: true,
+                message: 'Bạn chưa điền mã kì thi.',
+              },
+            ],
+          },
+        ]"
+      />
+    </a-form-item>
+    <a-form-item v-bind="formItemLayout" label="Tên kì thi">
+      <a-input style="width:200px"
+        v-decorator="[
+          'Tên kì thi',
+          {
+            rules: [
+              {
+                required: true,
+                message: 'Bạn chưa điền tên kì thi',
+              },
+            ],
+          },
+        ]"
+      />
+    </a-form-item>
+    <a-form-item v-bind="tailFormItemLayout">
+      <a-button type="primary" html-type="submit">
+        Tạo kì thi
+      </a-button>
+    </a-form-item>
+  </a-form>
+  </section>
+</template>
+
+<script>
+import Course_detail from "./Course_detail";
+export default {
+  name: "Course_creator",
+  components: {
+    Course_detail,
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ client/src/components/Admin/schedule_management/Shift_detail.vue
@@ -0,0 +1,39 @@
+<template>
+  <a-list size="small" bordered :dataSource="rooms">
+      <div slot="header">Danh sách ca thi</div>
+      <a-list-item slot="renderItem" slot-scope="room, index">
+        <div>
+          <a-select defaultValue="Chọn phòng thi" style="width: 200px" @change="handleChange">
+            <a-select-option v-for="room in rooms" value="room">{{room.name}}</a-select-option>
+          </a-select>
+          <a-date-picker style="margin-left: 5px; width: 200px" @change="onChange" />
+          <span>Thông tin ca thi:</span>
+        </div>
+      </a-list-item>
+    </a-list>
+</template>
+
+<script>
+export default {
+  name: "Shift_detail",
+  data() {
+    return {
+      rooms: [
+          {
+            name: "Phòng 408-G2",
+          },
+          {
+            name: "Phòng 320-GĐ3",
+          },
+          {
+            name: "Phòng 237-Overlook Hotel",
+          }
+      ]
+    }
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
