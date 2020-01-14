2a8cdcba5a84502180c2e908c99eae60c7ce7834
MinhPC <Needforspeed1900@gmail.com>
2020-01-14 19:34:11 +0700

    Removing all files in .gitignore

--- api/controller/time_conversion/asia_timezone.py
+++
@@ -1,8 +0,0 @@
-import pytz
-from datetime import datetime
-
-vntz = pytz.timezone('Asia/Ho_Chi_Minh')
-
-
-def set_custom_log_time():
-    return datetime.now(tz=vntz).strftime("%Y-%m-%d %H:%M:%S")
